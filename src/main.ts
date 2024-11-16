3cd0e4669fd447a7c35eca32a45ed206666f5967
Nemanja <ilicnemanja.it@gmail.com>
2024-11-16 09:22:52 +0100

    added cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,24 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        "http://localhost:3000",
+        "https://chatify-theta-olive.vercel.app/",
+      ];
+
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
   app.setGlobalPrefix("api");
 
   await app.listen(4000);
